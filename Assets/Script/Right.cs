cd29e6ccfb8e19db0e4a3e1981afdafcb637ab38
Remiel0306 <remiel.tsui@gmail.com>
2022-05-16 13:15:27 +0800

    Ground自動生成 刪除(有Bug))

---
+++ Assets/Script/Right.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// 讓軌道偵測後克隆
+/// </summary>
+namespace remiel
+{
+    public class Right : MonoBehaviour
+    {
+        [SerializeField] GameObject[] styles;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (other.name == "End")
+            {
+                if (other.gameObject.layer == LayerMask.NameToLayer("Ground"))
+                {
+                    GameObject clone = Instantiate(styles[Random.Range(0, styles.Length)], other.transform.position, Quaternion.identity);
+                    //Score[] scores = clone.GetComponentsInChildren<Score>();
+                    List<int> indexArray = new List<int>();
+                    //while (true)
+                    //{
+
+                    //}
+                }
+            }
+            else
+            {
+                Instantiate(other.transform.root.gameObject, other.transform.position, Quaternion.identity);
+            }
+        }
+    }
+}
