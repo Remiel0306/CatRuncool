9198af4d52a1e333bc291fd0c9bd80cd5fa51243
Remiel0306 <104558225+Remiel0306@users.noreply.github.com>
2022-06-05 13:03:26 +0800

    UI完成，重新開始有BUG

--- Assets/Script/UiCtrl.cs
+++ Assets/Script/UiCtrl.cs
@@ -11,7 +11,13 @@ namespace remiel
     public class UiCtrl : MonoBehaviour
     {
         [SerializeField] Button btnPlay;
-        [SerializeField] Button btnReplay;
+        [SerializeField] Button btnExit;
+        [SerializeField] Button btnSetting;
+        [SerializeField] GameObject settingPanel;
+        [SerializeField] GameObject firstPanel;
+        [SerializeField] Button btnSettingPanelBack;
+        [SerializeField] Button btnFinBackLobby;
+        [SerializeField] Button btnDeadBackLobby;
 
         private void Start()
         {
@@ -22,9 +28,36 @@ namespace remiel
             
         }
            
-        public void ButtonClick()
+        public void BtnPlayClick()
         {
             SceneManager.LoadScene("Game");
+        }
+
+        public void BtnExitClick()
+        {
+            Application.Quit();
+        }
+
+        public void BtnSettingClick()
+        {
+            settingPanel.SetActive(true);
+            firstPanel.SetActive(false);
+        }
+
+        public void BtnSettingPanelBackClick()
+        {
+            settingPanel.SetActive(false);
+            firstPanel.SetActive(true);
+        }
+
+        public void BtnBackLobbyClick()
+        {
+            SceneManager.LoadScene("Lobby");
+        }
+
+        public void BtnDeadBackLobbyClick()
+        {
+            SceneManager.LoadScene("Lobby");
         }
     }
 }
